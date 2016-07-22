d0cddf17c2fe4e80e48efd9c7991e9756c757608
Brian Polster <bpolster@broadleafcommerce.org>
2016-07-21 20:08:45 -0500

    Additional RequestLoggingUtility to show a general representation of the request id in case multiple developers are troubleshooting simoultaneously.   This code is for a specific troubleshooting problem with Buckle and not intended to be used generally without further design

--- common/src/main/java/org/broadleafcommerce/common/logging/RequestLoggingUtil.java
+++ common/src/main/java/org/broadleafcommerce/common/logging/RequestLoggingUtil.java
@@ -21,33 +21,54 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.common.web.BroadleafRequestContext;
 
+import java.util.Random;
 
 public class RequestLoggingUtil {
 
     public static final Log BL_OFFER_LOG = LogFactory.getLog("blOfferLog");
+    private static Random rand = new Random();
 
     public static void logInfoRequestMessage(String message, Log log) {
         if (isRequestLoggingEnabled()) {
-            log.info(message);
+            log.info(getRequestId() + message);
         }
     }
 
     public static void logDebugRequestMessage(String message, Log log) {
         if (isRequestLoggingEnabled()) {
-            log.debug(message);
+            log.debug(getRequestId() + message);
         }
     }
 
     public static void logTraceRequestMessage(String message, Log log) {
         if (isRequestLoggingEnabled()) {
-            log.trace(message);
+            log.trace(getRequestId() + message);
         }
     }
 
     public static void logWarnRequestMessage(String message, Log log) {
         if (isRequestLoggingEnabled()) {
-            log.warn(message);
+            log.warn(getRequestId() + message);
         }
+    }
+
+    /**
+     * Generally good request Id.    This accounts for a few developers testing with 
+     * request logging at the same time without requiring full on UUID strings in the logs.
+     * @return
+     */
+    public static String getRequestId() {
+        BroadleafRequestContext brc = BroadleafRequestContext.getBroadleafRequestContext();
+        if (brc != null && brc.getAdditionalProperties() != null) {
+            String id = (String) brc.getAdditionalProperties().get("BLC-DEBUG-requestId");
+            if (id == null) {
+                int requestId = rand.nextInt(9999);
+                id = requestId + "-";
+                brc.getAdditionalProperties().put("BLC-DEBUG-requestId", id);
+            }
+            return id;
+        }
+        return "";
     }
 
     public static boolean isRequestLoggingEnabled() {
