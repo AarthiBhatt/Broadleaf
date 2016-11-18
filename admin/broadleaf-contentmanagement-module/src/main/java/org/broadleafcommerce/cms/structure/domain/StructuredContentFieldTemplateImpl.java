e4d5fb3cd4b89d363efda92852d817eaf8793811
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-11-18 17:44:39 -0600

    Removed TODOs and uncommented code related to those TODOs revolving around locale, site, catalog, and multitenant cloneable

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/structure/domain/StructuredContentFieldTemplateImpl.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/structure/domain/StructuredContentFieldTemplateImpl.java
@@ -20,6 +20,7 @@ package org.broadleafcommerce.cms.structure.domain;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.Transformer;
 import org.broadleafcommerce.cms.field.domain.FieldGroup;
+import org.broadleafcommerce.cms.field.domain.FieldGroupImpl;
 import org.broadleafcommerce.common.presentation.AdminPresentation;
 import org.broadleafcommerce.common.presentation.AdminPresentationClass;
 import org.broadleafcommerce.common.presentation.PopulateToOneFieldsEnum;
@@ -128,21 +129,21 @@ public class StructuredContentFieldTemplateImpl implements StructuredContentFiel
         this.fieldGroupXrefs = fieldGroupXrefs;
     }
 
-// TODO microservices - deal with multitenant cloneable
-//    @Override
-//    public <G extends StructuredContentFieldTemplate> CreateResponse<G> createOrRetrieveCopyInstance(MultiTenantCopyContext context) throws CloneNotSupportedException {
-//        CreateResponse<G> createResponse = context.createOrRetrieveCopyInstance(this);
-//        if (createResponse.isAlreadyPopulated()) {
-//            return createResponse;
-//        }
-//        StructuredContentFieldTemplate cloned = createResponse.getClone();
-//        cloned.setName(name);
-//        for (StructuredContentFieldGroupXref entry : fieldGroupXrefs) {
-//            CreateResponse<StructuredContentFieldGroupXref> clonedGroupRsp = entry.createOrRetrieveCopyInstance(context);
-//            cloned.getFieldGroupXrefs().add(clonedGroupRsp.getClone());
-//        }
-//
-//        return createResponse;
-//    }
+    @Override
+    public <G extends StructuredContentFieldTemplate> CreateResponse<G> createOrRetrieveCopyInstance(MultiTenantCopyContext context) throws CloneNotSupportedException {
+        CreateResponse<G> createResponse = context.createOrRetrieveCopyInstance(this);
+        if (createResponse.isAlreadyPopulated()) {
+            return createResponse;
+        }
+        StructuredContentFieldTemplate cloned = createResponse.getClone();
+        cloned.setName(name);
+        for(FieldGroup entry : fieldGroups){
+            CreateResponse<FieldGroup> clonedGroupRsp = entry.createOrRetrieveCopyInstance(context);
+            FieldGroup clonedGroup = clonedGroupRsp.getClone();
+            cloned.getFieldGroups().add(clonedGroup);
+        }
+
+        return createResponse;
+    }
 }
 
