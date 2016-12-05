04b36166395835931cf532a7bd5971730d6e83ac
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-12-05 12:42:13 -0600

    BroadleafCommerce/QA#2457 - Added externalId methods to OrderCategory, OrderProduct, and OrderSku. Updated OrderItemService to use those methods to get the ids

--- order/src/main/java/com/broadleafcommerce/order/common/domain/OrderCategoryImpl.java
+++ order/src/main/java/com/broadleafcommerce/order/common/domain/OrderCategoryImpl.java
@@ -3,4 +3,10 @@ package com.broadleafcommerce.order.common.domain;
 
 public class OrderCategoryImpl implements OrderCategory {
 
+    protected Long externalId;
+    
+    @Override
+    public Long getExternalId() {
+        return externalId;
+    }
 }
--- order/src/main/java/com/broadleafcommerce/order/common/domain/OrderSkuImpl.java
+++ order/src/main/java/com/broadleafcommerce/order/common/domain/OrderSkuImpl.java
@@ -2,5 +2,9 @@ package com.broadleafcommerce.order.common.domain;
 
 
 public class OrderSkuImpl implements OrderSku {
-
+    protected Long externalId;
+    
+    public Long getExternalId() {
+        return externalId;
+    }
 }
