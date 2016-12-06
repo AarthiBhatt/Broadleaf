6606590df39446735c8ef7f3f0fb43f04159536a
Brandon Hines <brandon.d.hines@gmail.com>
2016-12-06 16:15:00 -0600

    BroadleafCommerce/QA#2457 - refactored order sku dto to use lombok

--- order/src/main/java/com/broadleafcommerce/order/common/dto/OrderSkuDTO.java
+++ order/src/main/java/com/broadleafcommerce/order/common/dto/OrderSkuDTO.java
@@ -17,11 +17,13 @@
  */
 package com.broadleafcommerce.order.common.dto;
 
+import lombok.Data;
+
 import org.broadleafcommerce.common.money.Money;
 
 import com.broadleafcommerce.order.common.domain.OrderSku;
 
-
+@Data
 public class OrderSkuDTO {
 
     protected String name;
@@ -31,68 +33,12 @@ public class OrderSkuDTO {
     protected boolean taxable;
     protected boolean discountable;
     protected boolean active;
-    
-    public OrderSku getSku() {
-        return sku;
-    }
-    
-    public void setSku(OrderSku sku) {
-        this.sku = sku;
-    }
-    
-    public Money getRetailPrice() {
-        return retailPrice;
-    }
-    
-    public void setRetailPrice(Money retailPrice) {
-        this.retailPrice = retailPrice;
-    }
-    
-    public Money getSalePrice() {
-        return salePrice;
-    }
-    
-    public void setSalePrice(Money salePrice) {
-        this.salePrice = salePrice;
-    }
-    
-    public boolean isTaxable() {
-        return taxable;
-    }
-    
-    public void setTaxable(boolean taxable) {
-        this.taxable = taxable;
-    }
-    
+
     public boolean hasRetailPrice() {
         return getRetailPrice() != null;
     }
-    
+
     public boolean hasSalePrice() {
         return getSalePrice() != null;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public boolean isDiscountable() {
-        return discountable;
-    }
-    
-    public void setDiscountable(boolean discountable) {
-        this.discountable = discountable;
-    }
-    
-    public boolean isActive() {
-        return active;
-    }
-    
-    public void setActive(boolean active) {
-        this.active = active;
     }
 }
