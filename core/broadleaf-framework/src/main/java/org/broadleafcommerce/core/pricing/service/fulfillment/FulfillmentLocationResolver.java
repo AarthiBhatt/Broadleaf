32d78c1e1f3bcd29025ffd3176370d45c6755834
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-12-08 13:32:41 -0600

    BroadleafCommerce/QA#2457 - Updated SimpleFulfillmentLocationResolver and FulfillmentLocationResolver to use OrderAddress instead of Address

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/pricing/service/fulfillment/FulfillmentLocationResolver.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/pricing/service/fulfillment/FulfillmentLocationResolver.java
@@ -18,7 +18,8 @@
 package org.broadleafcommerce.core.pricing.service.fulfillment;
 
 import org.broadleafcommerce.core.order.domain.FulfillmentGroup;
-import org.broadleafcommerce.profile.core.domain.Address;
+
+import com.broadleafcommerce.order.common.domain.OrderAddress;
 
 /**
  * This can be used by various third-party fulfillment pricing services in order to
@@ -33,13 +34,13 @@ import org.broadleafcommerce.profile.core.domain.Address;
 public interface FulfillmentLocationResolver {
 
     /**
-     * This method should give an {@link Address} that a particular {@link FulfillmentGroup} will
+     * This method should give an {@link OrderAddress} that a particular {@link FulfillmentGroup} will
      * be fulfilled from. Implementations could store this information in the database or integrate
      * with an existing warehouse solution.
      * 
      * @param group
-     * @return the {@link Address} that <b>group</b> should be fulfilled from
+     * @return the {@link OrderAddress} that <b>group</b> should be fulfilled from
      */
-    public Address resolveLocationForFulfillmentGroup(FulfillmentGroup group);
+    public OrderAddress resolveLocationForFulfillmentGroup(FulfillmentGroup group);
 
 }
