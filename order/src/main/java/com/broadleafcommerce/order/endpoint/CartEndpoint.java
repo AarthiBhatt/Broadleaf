fee74a889d2ad36d3ec06d79de4bbaf52315a635
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-12-09 17:21:54 -0600

    BroadleafCommerce/QA#2457 - Added basic cart operations endpoints such as add, remove, and update items and add, and remove promo codes

--- order/src/main/java/com/broadleafcommerce/order/endpoint/CartEndpoint.java
+++ order/src/main/java/com/broadleafcommerce/order/endpoint/CartEndpoint.java
@@ -1,8 +1,16 @@
 package com.broadleafcommerce.order.endpoint;
 
 import org.broadleafcommerce.common.controller.FrameworkRestController;
+import org.broadleafcommerce.core.offer.domain.OfferCode;
+import org.broadleafcommerce.core.offer.service.OfferService;
+import org.broadleafcommerce.core.offer.service.exception.OfferException;
 import org.broadleafcommerce.core.order.domain.Order;
 import org.broadleafcommerce.core.order.service.OrderService;
+import org.broadleafcommerce.core.order.service.call.OrderItemRequestDTO;
+import org.broadleafcommerce.core.order.service.exception.AddToCartException;
+import org.broadleafcommerce.core.order.service.exception.RemoveFromCartException;
+import org.broadleafcommerce.core.order.service.exception.UpdateCartException;
+import org.broadleafcommerce.core.pricing.service.exception.PricingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +34,9 @@ public class CartEndpoint {
     @Resource(name = "blOrderCustomerService")
     protected OrderCustomerService orderCustomerService;
     
+    @Resource(name = "blOfferService")
+    protected OfferService offerService;
+
     @RequestMapping(path = "/customer/{id}", method = RequestMethod.GET)
     public ResponseEntity findCartByCustomerId(HttpServletRequest request, @PathVariable Long id) {
         OrderCustomer customer = orderCustomerService.findOrderCustomerById(id);
@@ -51,4 +62,66 @@ public class CartEndpoint {
         }
         return new ResponseEntity(orderService.createNewCartForCustomer(customer), HttpStatus.OK);
     }
+    
+    @RequestMapping(path = "/{id}/add", method = RequestMethod.POST)
+    public ResponseEntity addItemToOrder(HttpServletRequest request, @PathVariable Long id, @RequestBody OrderItemRequestDTO dto) {
+        try {
+            return new ResponseEntity(orderService.addItem(id, dto, true), HttpStatus.OK);
+        } catch (AddToCartException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @RequestMapping(path = "/{orderId}/remove/{itemId}", method = RequestMethod.POST)
+    public ResponseEntity removeItemFromOrder(HttpServletRequest request, @PathVariable("orderId") Long orderId, @PathVariable("itemId") Long orderItemId) {
+        try {
+            return new ResponseEntity(orderService.removeItem(orderId, orderItemId, true), HttpStatus.OK);
+        } catch (RemoveFromCartException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @RequestMapping(path = "/{orderId}/update", method = RequestMethod.POST)
+    public ResponseEntity updateItemOnOrder(HttpServletRequest request, @PathVariable("orderId") Long orderId, @RequestBody OrderItemRequestDTO dto) {
+        try {
+            return new ResponseEntity(orderService.updateItemQuantity(orderId, dto, true), HttpStatus.OK);
+        } catch (UpdateCartException | RemoveFromCartException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @RequestMapping(path = "/{orderId}/add/promo/{promoCode}", method = RequestMethod.POST)
+    public ResponseEntity addPromoToOrder(HttpServletRequest request, @PathVariable("orderId") Long orderId, @PathVariable("promoCode") String promoCode) {
+        Order order = orderService.findOrderById(orderId);
+        if (order == null) {
+            return new ResponseEntity("No order exists for id " + orderId, HttpStatus.BAD_REQUEST);
+        }
+        OfferCode offerCode = offerService.lookupOfferCodeByCode(promoCode);
+        if (offerCode == null) {
+            return new ResponseEntity("No offerCode exists for offer code " + promoCode, HttpStatus.BAD_REQUEST);
+        }
+        try {
+            return new ResponseEntity(orderService.addOfferCode(order, offerCode, true), HttpStatus.OK);
+        } catch (OfferException | PricingException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @RequestMapping(path = "/{orderId}/remove/promo/{promoCode}", method = RequestMethod.POST)
+    public ResponseEntity removePromoFromOrder(HttpServletRequest request, @PathVariable("orderId") Long orderId, @PathVariable("promoCode") String promoCode) {
+        Order order = orderService.findOrderById(orderId);
+        if (order == null) {
+            return new ResponseEntity("No order exists for id " + orderId, HttpStatus.BAD_REQUEST);
+        }
+        OfferCode offerCode = offerService.lookupOfferCodeByCode(promoCode);
+        if (offerCode == null) {
+            return new ResponseEntity("No offerCode exists for offer code " + promoCode, HttpStatus.BAD_REQUEST);
+        }
+        try {
+            return new ResponseEntity(orderService.removeOfferCode(order, offerCode, true), HttpStatus.OK);
+        } catch (PricingException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
 }
